bb5b0473f5463d2ec7351cc83949da6784adf230
hrwhisper <hrwhisper@qq.com>
2016-04-23 17:02:09 +0800

    new navigation and  sentiment/topic page . All charts background to transparent

--- sentiment/urls.py
+++ sentiment/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-import statistic.views
+import sentiment.views
 
 urlpatterns = [
-
+    url(r'^$', sentiment.views.index),
 ]
--- sentiment/views.py
+++ sentiment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'sentiment/index.html')
+
