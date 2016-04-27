19e353dde2e47276a56b1c64c9249fa6f9a3bb41
hrwhisper <hrwhisper@qq.com>
2016-04-27 11:19:36 +0800

    add google map for location select

---
+++ static/js/googleMap.js
@@ -0,0 +1,83 @@
+var googleMap = {
+    map: null,
+    markers: {},
+    currentId: 0,
+
+    uniqueId: function () {
+        return ++this.currentId;
+    },
+
+    infowindow: new google.maps.InfoWindow({
+        size: new google.maps.Size(150, 50)
+    }),
+
+
+    initialize: function () {
+        if (this.map) return null;
+
+        var myOptions = {
+            zoom: 2,
+            center: new google.maps.LatLng(43.907787, 0),
+            mapTypeControl: true,
+            mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+            navigationControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        this.map = new google.maps.Map(document.getElementById("map_canvas"),
+            myOptions);
+
+        google.maps.event.addListener(this.map, 'click', function () {
+            googleMap.infowindow.close();
+        });
+
+        google.maps.event.addListener(this.map, 'click', function (event) {
+            var Latitude = event.latLng.lng().toFixed(2);
+            var longitude = event.latLng.lat().toFixed(2);
+            googleMap.addMarker(event.latLng, "name", "<b>Location</b><br>" +Latitude +","+ longitude,
+                longitude + ',' + Latitude);
+        });
+
+        //google.maps.event.addListener(this.map, 'click', function (event) {
+        //    console.log("Latitude: " + event.latLng.lat() + " " + ", longitude: " + event.latLng.lng());
+        //});
+    },
+
+
+    addMarker: function (Gpoint, name, contentString, geo) {
+        var id = this.uniqueId(); // get new id
+        marker = new google.maps.Marker({
+            id: id,
+            position: Gpoint,
+            geo: geo,
+            map: googleMap.map,
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            googleMap.infowindow.setPosition(this.position);
+            googleMap.infowindow.setContent(contentString);
+            googleMap.infowindow.open(googleMap.map, marker);
+        });
+        google.maps.event.trigger(marker, 'click');
+
+        googleMap.map.panTo(Gpoint);
+
+        this.markers[id] = marker;
+
+        google.maps.event.addListener(marker, "rightclick", function (point) {
+            googleMap.delMarker(this.id)
+        });
+        //var res = '';
+        //for (i in googleMap.markers){
+        //   res += googleMap.markers[i].geo + ',';
+        //}
+        //res = res.substring(0,res.length-1)
+        //console.log(res);
+    },
+
+    delMarker: function (id) {
+        this.markers[id].setMap(null);
+        delete  this.markers[id];
+    }
+};
--- twitterDataMining/model_p/twitterApi/Stream.py
+++ twitterDataMining/model_p/twitterApi/Stream.py
@@ -18,15 +18,15 @@ class TwitterStream(TwitterBasic):
     def ready_receive(self):
         self.get_data = True
 
-    def stream_data(self, track_list=None, follow_list=None, locations_list=None, save_to_db=False,
+    def stream_data(self, track=None, follow=None, locations=None, save_to_db=False,
                     collection_name='stream'):
         """
             https://dev.twitter.com/streaming/reference/post/statuses/filter
             The default access level allows up to 400 track keywords, 5,000 follow userids and 25 0.1-360 degree location boxes.
 
-        :param track_list:str list      ;
-        :param follow_list:list (str list or int list ) ; usr_id list
-        :param locations_list: list ; geo list
+        :param track: str    ;
+        :param follow:list str ;
+        :param locations: str ;
         :param save_to_db:
         :param collection_name:
         :return: None
@@ -34,24 +34,24 @@ class TwitterStream(TwitterBasic):
 
         kwg = {'language': 'en'}
 
-        if not track_list and not follow_list and not locations_list:
+        if not track and not follow and not locations:
             kwg['track'] = 'twitter'
 
-        if track_list:
-            kwg['track'] = ','.join(track for track in track_list)
+        if track:
+            kwg['track'] = track
 
-        if follow_list:
-            kwg['follow'] = ','.join(str(follow) for follow in follow_list)
+        if follow:
+            kwg['follow'] = follow
 
-        if locations_list:
-            kwg['locations'] = ','.join(str(locations) for locations in locations_list)
+        if locations:
+            kwg['locations'] = locations
 
         twitter_stream = twitter.TwitterStream(auth=self.twitter_api.auth)
         stream = twitter_stream.statuses.filter(**kwg)
         print kwg
 
         for i, tweet in enumerate(stream):
-            if not i % 500: print i, datetime.datetime.now(), ' ', tweet
+            print i, datetime.datetime.now(), ' ', tweet
             tweet = dict(tweet)
             if 'id' in tweet:
                 self.tweets.append(tweet)
@@ -69,17 +69,18 @@ if __name__ == '__main__':
     def get_current_time():
         error_time = int(time.time())  # ->这是时间戳
         error_time = time.localtime(error_time)
-        otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", error_time)
-        return otherStyleTime
+        other_style_time = time.strftime("%Y-%m-%d %H:%M:%S", error_time)
+        return other_style_time
 
 
     t = TwitterStream()
-    track_list = None
+    track = None
+    locations = '-122.75,36.8,-121.75,37.8,-74,40,-73,41'
     while True:
         try:
-            t.stream_data(track_list=track_list, save_to_db=False, colname='stream')
+            t.stream_data(track=track, locations=locations, save_to_db=False)
         except Exception, e:
             with open('error_log.txt', 'a+') as f:
-                erro_info = get_current_time() + '    ' + str(e) + ' \n'
-                print erro_info
-                f.write(erro_info)
+                error_info = get_current_time() + '    ' + str(e) + ' \n'
+                print error_info
+                f.write(error_info)
